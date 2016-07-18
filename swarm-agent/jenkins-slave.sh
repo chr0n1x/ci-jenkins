da5cf1c24918d67ff25f995600f278fc87cd5be2
chr0n1x <heilong24@gmail.com>
2016-07-18 14:41:37 -0400

    swarm-agent: throw into d-in-d container, move to alpine

--- swarm-agent/jenkins-slave.sh
+++ swarm-agent/jenkins-slave.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+set -e
+echo "==> Launching the Docker daemon..."
+dind docker daemon --host=unix:///var/run/docker.sock --storage-driver=vfs &
+while(! docker info > /dev/null 2>&1); do
+    echo "==> Waiting for the Docker daemon to come online..."
+    sleep 1
+done
+echo "==> Docker Daemon is up and running!"
+apk add make
+
 # if `docker run` first argument start with `-` the user is passing jenkins swarm launcher arguments
 if [[ $# -lt 1 ]] || [[ "$1" == "-"* ]]; then
-
   # jenkins swarm slave
   JAR=`ls -1 /usr/share/jenkins/swarm-client-*.jar | tail -n 1`
-
   # if -master is not provided and using --link jenkins:jenkins
   if [[ "$@" != *"-master "* ]] && [ ! -z "$JENKINS_TCP_ADDR" ]; then
     PARAMS="-master http://$JENKINS_TCP_ADDR:$JENKINS_TCP_PORT"
   fi
-
   echo Running java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
   exec java $JAVA_OPTS -jar $JAR -fsroot $HOME $PARAMS "$@"
 fi
